c1b22d9009612863d34bb50bc5334836842374fa
Leanderson Anadao <anadao2@gmail.com>
2019-11-08 23:20:52 -0300

    Ajustando as validações de CPF, Endereço e EMAIL, mudando também os tipos de retorno do Flask

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
-# GET  /api/v1/customers - listar os clientes
-# GET  /api/v1/customers/`<key>` - detalhe do cliente
-# POST /api/v1/customers - cadastrar um novo cliente
+import json
+
 import jsons
+import pymongo
 from flask import Flask, jsonify, request
+from flask_api import status
 
 from classes.address import Address
 from classes.customer import Customer
@@ -13,25 +14,57 @@ app = Flask(__name__)
 
 @app.route('/api/v1/customers', methods=['GET'])
 def customers():
-    data = Customer.list()
-    return jsons.dump(data)
+    try:
+        data = Customer.list()
+        status_code = status.HTTP_200_OK
+
+    except Exception as ex:
+        content = {'message': ex.args}
+        status_code = status.HTTP_404_NOT_FOUND
+
+    return json.dumps(data), status_code, {'ContentType': 'application/json'}
 
 
 @app.route('/api/v1/customer/<cpf>', methods=['GET'])
 def customerByCPF(cpf):
-    data = Customer.get_customer_by_cpf(cpf)
-    return jsons.dump(data)
+    try:
+        data = Customer.get_customer_by_cpf(cpf)
+        status_code = status.HTTP_200_OK
+
+    except Exception as ex:
+        content = {'message': ex.args}
+        status_code = status.HTTP_404_NOT_FOUND
+
+    return json.dumps(data), status_code, {'ContentType': 'application/json'}
 
 
 @app.route('/api/v1/new_customer', methods=['POST'])
 def newCustomer():
-    print(request.json)
+    status_code = ""
+    content = ""
     req_data = request.get_json()
-    address = Address(req_data['cep'], req_data['numero'], req_data['complemento'])
-    customer = Customer(req_data['nome'], req_data['email'], req_data['cpf'], address, req_data['telefone'])
-    print(customer)
-    customer.save()
-    return customer.cpf + " cadastrado"
+
+    try:
+        address = Address(req_data['cep'], req_data['numero'], req_data['complemento'])
+        customer = Customer(req_data['nome'], req_data['email'], req_data['cpf'], address, req_data['telefone'])
+        customer.save()
+        content = {'message': 'Cadastrado com sucesso'}
+        status_code = status.HTTP_201_CREATED
+
+    except pymongo.errors.DuplicateKeyError:
+        content = {'message': 'CPF existente'}
+        status_code = status.HTTP_409_CONFLICT
+        # HTTP_500_INTERNAL_SERVER_ERROR
+
+    except ValueError as ex:
+        content = {'message': 'Numero invalido'}
+        status_code = status.HTTP_404_NOT_FOUND
+
+    except Exception as ex:
+        content = {'message': ex.args}
+        status_code = status.HTTP_404_NOT_FOUND
+
+    return json.dumps(content), status_code, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
